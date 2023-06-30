ff8771095b5a750a8a33c8b52e8d102bd20a8167
rezamohdev <mohammadireza29@gmail.com>
2023-06-29 21:33:54 -0400

    user card map function

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import './App.css';
 import Header from './Header';
 import UserCard from './UserCard';
 import ChatPanel from './ChatPanel';
+import chats from './chats';
 
 function App() {
   return (
     <div className="App">
       <div className='sidebar'>
         <Header />
-        <UserCard time="6:30" userName="Reza Mohammadi" seen={true} delivered={true} attach="" message={"Hi my dear friend"} />
-        <UserCard time="2:00" userName="John Doe" seen={true} delivered={true} attach="" message={"Hi my dear friend"} />
-        <UserCard time="1:56" userName="Michael " seen={true} delivered={true} attach="" message={"Hi my dear friend"} />
-        <UserCard time="12:00" userName="Lisa" seen={true} delivered={true} attach="voice" message={""} />
-        <UserCard time="18:45" userName="Unkown user" seen={true} delivered={true} attach="" message={"Hi my dear friend"} />
+        {chats.map((chat) => (<UserCard time={chat.time} userName={chat.username} seen={true} delivered={true} attach={chat.attach} message={chat.message} />)
+        )}
       </div>
       <ChatPanel />
     </div>
---
+++ src/chats.js
@@ -0,0 +1,68 @@
+const chats = [{
+    id: 1,
+    username: 'John Doe',
+    time: '20:45',
+    seen: true,
+    delivered: false,
+    attach: "image",
+    message: "Hi how u doin?"
+}, {
+    id: 2,
+    username: 'Reza',
+    time: '20:45',
+    seen: true,
+    delivered: false,
+    attach: "video",
+    message: "Hi how u doin?"
+}, {
+    id: 3,
+    username: 'Your Friend',
+    time: '20:45',
+    seen: true,
+    delivered: false,
+    attach: "",
+    message: "Hi how u doin?"
+}, {
+    id: 4,
+    username: 'Gregory',
+    time: '20:45',
+    seen: true,
+    delivered: false,
+    attach: "voice",
+    message: "Hi how u doin?"
+}, {
+    id: 5,
+    username: 'Gregory',
+    time: '20:45',
+    seen: true,
+    delivered: false,
+    attach: "",
+    message: "Hi how u doin?"
+}, {
+    id: 6,
+    username: 'Gregory',
+    time: '20:45',
+    seen: true,
+    delivered: false,
+    attach: "",
+    message: "Hi how u doin?"
+}, {
+    id: 7,
+    username: 'Gregory',
+    time: '20:45',
+    seen: true,
+    delivered: false,
+    attach: "voice",
+    message: "Hi how u doin?"
+}, {
+    id: 8,
+    username: 'Gregory',
+    time: '20:45',
+    seen: true,
+    delivered: false,
+    attach: "",
+    message: "Hi how u doin?"
+},
+];
+
+export default chats;
